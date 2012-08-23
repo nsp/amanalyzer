c5cf8ce01fdc13ca195e663333ddab651c83e926
Nate Parsons <parsons.nate@gmail.com>
2012-08-23 16:57:35 -0400

    Fix timestamps, print statement
    Previous issue with "instant" posts was that each run of this script
    modifies atimes, so scrape needs to be called in between

--- gettldata.py
+++ gettldata.py
@@ -12,20 +12,20 @@ def td_secs(td):
     return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
 
 if __name__ == '__main__':
-    lastt = datetime(2012, 8, 22, 18, 16, 41)
+    lastt = datetime.utcfromtimestamp(1345659401.0)
     for (root, dirs, fnames) in os.walk('comments'):
         paths = (os.path.join(root, t_id) for t_id in fnames)
         for p in sorted(paths, key=get_t):
             t_id = os.path.basename(p)
-            thist = datetime.fromtimestamp(get_t(p))
-            dt_mins = 60 * td_secs(thist - lastt)
+            thist = datetime.utcfromtimestamp(get_t(p))
+            dt_mins = td_secs(thist - lastt) / 60.0
             text = linkbase % t_id
-            words = 0
+            words = 0.0
             with open(p) as f:
                 body = f.read().decode('utf-8')
                 words += len(re.findall('[A-Za-z0-9]+', body))
             wpm = words / dt_mins
-            print "[new Date(%s), %d, %s, %s, %f, %s, %s]," % \
+            print "[new Date(%s), %d, '%s', '%s', %f, %s, %s]," % \
                 (thist.strftime('%Y, %m, %d, %H, %M, %S'), \
                      words, \
                      t_id, \

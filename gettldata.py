9bd794e8b6069e6cd64109c0a2d30f6f88f4355d
Nate Parsons <parsons.nate@gmail.com>
2012-08-23 15:50:16 -0400

    fixing some bugs, now have a mysterious instantaneous post

--- gettldata.py
+++ gettldata.py
@@ -1,11 +1,12 @@
-import os, os.path
+import os, os.path, re
 from datetime import datetime, timedelta
 
 linkbase = u'<a href="http://reddit.com/r/IAmA/comments/yne9x/i_am_dan_harmon_creator_of_community_writer_of/%s?context=1">Permalink</a>'
 
 def get_t(path):
     """Apparently os.utime() only sets the st_atime on my system. Workaround"""
-    min(statinf.st_atime, statinf.st_mtime, statinf.st_ctime)
+    statinf = os.stat(path)
+    return min(statinf.st_atime, statinf.st_mtime, statinf.st_ctime)
     
 def td_secs(td):
     return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
@@ -13,20 +14,23 @@ def td_secs(td):
 if __name__ == '__main__':
     lastt = datetime(2012, 8, 22, 18, 16, 41)
     for (root, dirs, fnames) in os.walk('comments'):
-        paths = (os.path.join(root, id) for fname in fnames)
+        paths = (os.path.join(root, t_id) for t_id in fnames)
         for p in sorted(paths, key=get_t):
+            t_id = os.path.basename(p)
             thist = datetime.fromtimestamp(get_t(p))
-            text = linkbase % id
+            dt_mins = 60 * td_secs(thist - lastt)
+            text = linkbase % t_id
             words = 0
             with open(p) as f:
-                body = p.read().decode('utf-8')
-                words += len(re.findall('[A-Za-z0-9]+'))
+                body = f.read().decode('utf-8')
+                words += len(re.findall('[A-Za-z0-9]+', body))
+            wpm = words / dt_mins
             print "[new Date(%s), %d, %s, %s, %f, %s, %s]," % \
-                thist.strftime('%Y, %m, %d, %H, %M, %S'), \
-                words, \
-                id, \
-                text, \
-                words / td_secs(thist - lastt), \
-                'undefined', 'undefined'
+                (thist.strftime('%Y, %m, %d, %H, %M, %S'), \
+                     words, \
+                     t_id, \
+                     text, \
+                     wpm, \
+                     'undefined', 'undefined')
             lastt = thist
                 
